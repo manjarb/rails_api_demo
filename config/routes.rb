6787be31a7cecde713d89869c38b4f8e34dede85
manjarb <manjarb4@gmail.com>
2016-08-10 16:42:28 +0700

    add d

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
+  devise_for :users
   namespace :api , defaults: { format: :json },
                                constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1 ,
