2572dbd6c1b5a80115ed3ad5f2b2425b90895385
manjarb <manjarb4@gmail.com>
2016-08-11 20:22:23 +0700

    bent

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,4 +16,19 @@ class Api::V1::UsersController < ApplicationController
 
   end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
