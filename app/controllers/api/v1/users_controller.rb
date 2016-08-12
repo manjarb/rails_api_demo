ca2c9a7fd9cdf2283843c22a64244edc3212d39f
manjarb <manjarb4@gmail.com>
2016-08-13 00:11:15 +0700

    dtdt

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
+  before_action :authenticate_with_token!, only: [:update, :destroy]
+
   respond_to :json
 
   def show
@@ -26,19 +28,32 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
+    # user = User.find(params[:id])
+    #
+    # if user.update(user_params)
+    #   render json: user, status: 200, location: [:api, user]
+    # else
+    #   render json: { errors: user.errors }, status: 422
+    # end
+
+    user = current_user
 
     if user.update(user_params)
       render json: user, status: 200, location: [:api, user]
     else
       render json: { errors: user.errors }, status: 422
     end
+
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
+    # user = User.find(params[:id])
+    # user.destroy
+    # head 204
+
+    current_user.destroy
     head 204
+
   end
 
   private
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -57,6 +57,12 @@ describe Api::V1::UsersController do
 
   describe "PUT/PATCH #update" do
 
+    before(:each) do
+      @user = FactoryGirl.create :user
+      api_authorization_header @user.auth_token #we added this line
+      #request.headers['Authorization'] =  @user.auth_token
+    end
+
     context "when is successfully updated" do
       before(:each) do
         @user = FactoryGirl.create :user
@@ -95,8 +101,13 @@ describe Api::V1::UsersController do
 
   describe "DELETE #destroy" do
     before(:each) do
+      # @user = FactoryGirl.create :user
+      # delete :destroy, { id: @user.id }
+
       @user = FactoryGirl.create :user
-      delete :destroy, { id: @user.id }
+      api_authorization_header @user.auth_token #we added this line
+      delete :destroy, id: @user.auth_token
+
     end
 
     it { should respond_with 204 }
